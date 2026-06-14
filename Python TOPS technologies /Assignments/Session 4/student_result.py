ca4285054bbbe9684ae1054aba32e996b4a76ef9
yugakbari3-jpg <yugakbari3@gmail.com>
2026-06-14 19:55:51 +0530

    Add files via upload

---
+++ Python TOPS technologies /Assignments/Session 4/student_result.py
@@ -0,0 +1,12 @@
+mark = int(input("Enter your marks between 0 and 100:"))
+
+if mark >=85:
+    print("Result: Grade A")
+elif mark >=70:
+    print("Result: Grade B")
+elif mark >=55:
+    print("Result: Grade C")
+elif mark>=40:
+    print("Result: Grade B")
+else:
+    print("Result: Fail")
