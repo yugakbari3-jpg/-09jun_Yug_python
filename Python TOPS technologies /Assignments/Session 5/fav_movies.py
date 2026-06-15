5d208c462dd89b24a01ce99b1181921d908c9ac3
yugakbari3-jpg <yugakbari3@gmail.com>
2026-06-15 12:16:01 +0530

    Add files via upload

---
+++ Python TOPS technologies /Assignments/Session 5/fav_movies.py
@@ -0,0 +1,4 @@
+fav_movies = ("Housefull","Ironman","Avengers Infinity War")
+
+for string in fav_movies:
+    print(string.upper())
---
+++ Python TOPS technologies /Assignments/Session 5/names_food_delivery.py
@@ -0,0 +1,5 @@
+names = (" Zomato", "Blinkit", "Swiggy", "Zepto", "Instamart") #This is a Tuple
+
+for string in names:
+    print(string.upper())
+
