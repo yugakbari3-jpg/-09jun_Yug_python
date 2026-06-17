fe40654510b407ef356106b3b1ded465abfcad68
yugakbari3-jpg <yugakbari3@gmail.com>
2026-06-17 12:53:19 +0530

    Add files via upload

---
+++ Python TOPS technologies /Assignments/Session 6/stra_pattern.py
@@ -0,0 +1,5 @@
+print("Book my show seat rows:")
+rows = 1 
+while rows <= 7:
+    print("*" *rows)
+    rows+=2
---
+++ Python TOPS technologies /Assignments/Session 6/zomato_countdown.py
@@ -0,0 +1,4 @@
+i = 5 
+while i >=0:
+    print(f"Offer ends in {i} minutes ")
+    i-=1
