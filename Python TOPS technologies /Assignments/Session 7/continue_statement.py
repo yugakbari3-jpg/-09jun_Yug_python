d45979f501b4b7a8c1c079cac80b7da6ae1a0618
yugakbari3-jpg <yugakbari3@gmail.com>
2026-06-19 13:18:27 +0530

    Add files via upload

---
+++ Python TOPS technologies /Assignments/Session 7/continue_statement.py
@@ -0,0 +1,5 @@
+Fruits = ['Apple', 'Banana', 'Mango', 'Orange']
+for Fruits in Fruits:
+    if Fruits == 'Banana':
+       continue
+    print(Fruits)
---
+++ Python TOPS technologies /Assignments/Session 7/pass_statement.py
@@ -0,0 +1,9 @@
+song = ['Chill Vibes', 'Workout', 'Focus', 'Party']
+for song in song:
+    if song == 'Focus':
+     pass
+    else:
+      print(song)
+     
+
+     
