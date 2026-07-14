8b83fc7b585bf415dd66a519a8160b96b58b26bc
yugakbari3-jpg <yugakbari3@gmail.com>
2026-07-14 11:48:18 +0530

    Delete Python TOPS technologies /Assignments/Session 11 directory

--- Python TOPS technologies /Assignments/Session 11/dict_example1.py
+++
@@ -1,5 +0,0 @@
-my_playlist = {'Tum hi ho Bnadhu:3min',
-               'Cosmo:4min',
-               'Rubicon Drill:3.24'}
-
-print(my_playlist)
--- Python TOPS technologies /Assignments/Session 11/food_order.py
+++
@@ -1,4 +0,0 @@
-Order = {'Pizza': 2, 'Burger': 1, 'Fries': 3}
-print(Order.keys())
-print(Order.values())
-print(Order.items())
