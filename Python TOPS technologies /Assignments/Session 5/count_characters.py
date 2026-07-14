7fc85d54f68aa11c87c2cced9f3d01494047f33e
yugakbari3-jpg <yugakbari3@gmail.com>
2026-07-14 11:47:57 +0530

    Delete Python TOPS technologies /Assignments/Session 5 directory

--- Python TOPS technologies /Assignments/Session 5/count_characters.py
+++
@@ -1,8 +0,0 @@
-user_bio = ("Music lover, Traveller, Foodie ")
-
-count = 0
-for char in user_bio:
-    if char != " ":
-     count += 1
-
-print(count)
--- Python TOPS technologies /Assignments/Session 5/names_food_delivery.py
+++
@@ -1,5 +0,0 @@
-names = (" Zomato", "Blinkit", "Swiggy", "Zepto", "Instamart") #This is a Tuple
-
-for string in names:
-    print(string.upper())
-
